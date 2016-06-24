33d754037459ea39ab4e61ccf453dda1004e8963
Isaac Hoffmann <ihoffmann@Isaacs-MacBook-Pro.local>
2016-06-24 14:13:20 -0500

    Removed stuff to try and build a simpler app

--- webpack.config.js
+++ webpack.config.js
@@ -1,134 +1,29 @@
-'use strict';
+var path = require('path')
+var webpack = require('webpack')
 
-const webpack = require('webpack');
-const path = require('path');
-const autoprefixer = require('autoprefixer');
-const env = require('yargs').argv.build;
-
-const configEnvVals = {};
-configEnvVals.filename = 'publishing-ui.min';
-configEnvVals.entry = path.join(__dirname, '/app/scripts/bootstrap');
-configEnvVals.cssModuleLoaderLocalIndent = 'css?modules&importLoaders=1!postcss!sass';
-
-//always present, environment based config vals
-if (env === 'dev') {
-    console.log('\x1b[34m%s\x1b[0m', '----- Building Publishing-UI in DEV -----');
-    configEnvVals.babelCacheDir = true;
-    configEnvVals.textLoaderTest = 'url';
-    configEnvVals.filename = 'publishing-ui';
-    configEnvVals.entry = path.join(__dirname, '/app/scripts/testbed');
-    configEnvVals.cssModuleLoaderLocalIndent = 'style!css?modules&importLoaders=1&localIdentName=[path][name]---[local]---[hash:base64:5]!postcss!sass';
-} else {
-    console.log('\x1b[32m%s\x1b[0m', '----- Building Publishing-UI in PROD -----');
-    configEnvVals.textLoaderTest = 'file-loader?name=assets/[name]-[hash].[ext]';
-    configEnvVals.babelCacheDir = false;
+module.exports = {
+  devtool: 'cheap-module-eval-source-map',
+  entry: [
+    'webpack-hot-middleware/client',
+    './index'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  plugins: [
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: [ 'babel' ],
+        exclude: /node_modules/,
+        include: __dirname
+      }
+    ]
+  }
 }
-
-//base webpack config
-let config = {
-    entry: [configEnvVals.entry],
-    output: {
-        path: __dirname + '/lib',
-        filename: `${configEnvVals.filename}.js`,
-        umdNamedDefine: true
-    },
-    resolve: {
-        root: [
-            path.join(__dirname, '/app/scripts')
-        ],
-        fallback: [
-            path.join(__dirname, '/node_modules')
-        ],
-        alias: {
-            'styles': path.join(__dirname, '/app/styles'),
-            'scripts': path.join(__dirname, '/app/scripts'),
-            'images': path.join(__dirname, '/app/images')
-        }
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                exclude: /\/node_modules\//,
-                loader: 'babel',
-                query: {
-                    presets: ['react', 'es2015', 'stage-0'],
-                    plugins: [
-                        'transform-runtime',
-                        'transform-decorators-legacy',
-                        'lodash'
-                    ]
-                },
-                cacheDirectory: configEnvVals.babelCacheDir
-            },
-            {
-                test: /\.scss$/,
-                exclude: [
-                    path.join(__dirname, 'app/styles/modules')
-                ],
-                loader: 'style!css!postcss!sass'
-            },
-            {
-                test: /\.css$/,
-                loader: 'style!css!postcss'
-            },
-            {
-                test: /\.scss$/,
-                include: [
-                    path.join(__dirname, 'app/styles/modules')
-                ],
-                loader: configEnvVals.cssModuleLoaderLocalIndent
-            },
-            {
-                test: /\.(woff|woff2|ttf|eot|svg|png|gif|jpg)$/,
-                loader: configEnvVals.textLoaderTest
-            }
-        ]
-    },
-    plugins: [
-        new webpack.optimize.OccurrenceOrderPlugin()
-    ],
-    postcss: function() {
-        return [autoprefixer({
-            browsers: [
-                'last 3 versions',
-                '> 1%',
-                'ie >= 10'
-            ],
-            cascade: false
-        })];
-    }
-};
-
-//environment based config plugins
-if (env === 'dev') {
-    config.module.devtool = 'source-map';
-    config.module.loaders[0].query.plugins.push(new webpack.NoErrorsPlugin());
-    if (process.env.HOT) {
-        config.module.loaders[0].query.plugins.push(['react-transform', {
-            'transforms': [{
-                transform: 'react-transform-hmr',
-                imports: ['react'],
-                locals: ['module']
-            }, {
-                transform: 'react-transform-catch-errors',
-                imports: ['react', 'redbox-react']
-            }]
-        }]);
-    }
-} else {
-    config.module.loaders[0].query.plugins.push('transform-react-constant-elements');
-    config.plugins.push(new webpack.optimize.UglifyJsPlugin({
-        compress: {
-            drop_console: true
-        }
-    }));
-    config.plugins.push(new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /en/));
-    config.plugins.push(new webpack.optimize.DedupePlugin());
-    config.externals = {
-        'react': 'react',
-        'react-dom': 'react-dom'
-    };
-}
-
-module.exports = config;
