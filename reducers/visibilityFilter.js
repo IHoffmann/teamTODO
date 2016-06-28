9670c1e84e91c7dc12cf8925e5130062baa8f69a
Isaac Hoffmann <ihoffmann@Isaacs-MacBook-Pro.local>
2016-06-28 09:40:34 -0500

    implemented the visibilityFilter reducer

--- reducers/visibilityFilter.js
+++ reducers/visibilityFilter.js
@@ -2,14 +2,14 @@
  * Created by ihoffmann on 6/24/16.
  */
 import {SET_VIS} from '../actions/actions';
-//TODO ADD this functionality
+
 const visibilityFilter = (state = 'SHOW_ALL', action) => {
     switch (action.type) {
         case SET_VIS:
-            return action.filter
+            return action.filter;
         default:
-            return state
+            return state;
     }
-}
+};
 
-export default visibilityFilter
+export default visibilityFilter;
